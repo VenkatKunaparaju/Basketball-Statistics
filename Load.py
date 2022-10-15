b06f35a6318e77bd69d55eeeed0bf34fe025c12a
VenkatKunaparaju <venkatkrox@gmail.com>
2022-10-15 15:20:45 -0400

    Update Load.py

--- Load.py
+++ Load.py
@@ -1,3 +1,6 @@
+#File to establish mysql connection, connect to database, and load in tables and data if needed
+
+    #TODO compile data into csv and load it into respective table
 import mysql.connector
 
 
@@ -27,7 +30,7 @@ if 'Basketball' not in check:
     cursor.execute( #Create Player table
     """
     CREATE TABLE Player (
-        id INT AUTO_INCREMENT, Year INT, Name VARCHAR(255), Team VARCHAR(255), 
+        id VARCHAR(255), Year INT, Name VARCHAR(255), Team VARCHAR(255), 
         Age INT, Position VARCHAR(255), 
         PRIMARY KEY (id, Year)
     )
@@ -37,7 +40,7 @@ if 'Basketball' not in check:
     cursor.execute( #Create Season Stats table
     """
     CREATE TABLE Season_stats (
-        id INT AUTO_INCREMENT, Year INT, Games_played INT, Games_started INT, Minutes_played INT, Made_field_goals INT, 
+        id VARCHAR(255), Year INT, Games_played INT, Games_started INT, Minutes_played INT, Made_field_goals INT, 
         Attempted_field_goals INT, Made_3_pts INT, Attempted_3_pts INT, Made_ft INT, Attempted_ft INT, Offensive_rebounds INT,
         Defensive_rebounds INT, Assists INT, Steals INT, Blocks INT, Turnovers INT, Fouls INT, Points INT,
         PRIMARY KEY (id, Year)
@@ -45,16 +48,28 @@ if 'Basketball' not in check:
     """
     )
 
+    cursor.execute( #Create Game Stats table
+    """
+    CREATE TABLE Game_stats (
+        id VARCHAR(255), Year INT, GameNum INT, Games_played INT, Outcome VARCHAR(255), Opponent VARCHAR(255), Team VARCHAR(255),
+        Date VARCHAR(255), Location VARCHAR(255), Active VARCHAR(255), 
+        Seconds_played INT,  Made_field_goals INT, Attempted_field_goals INT, Made_3_pts INT, Attempted_3_pts INT, 
+        Made_ft INT, Attempted_ft INT, Offensive_rebounds INT, Defensive_rebounds INT, 
+        Assists INT, Steals INT, Blocks INT, Turnovers INT, Fouls INT, Points INT, Game_score INT, Plus_minus INT,
+        PRIMARY KEY (id, Year, GameNum)
+    )
     
-
-
-
-
+    """
+    )
 
 else: # Use 'Basketball' if it already exists
     cursor.execute("USE Basketball")
 
+
+#Debug: Check if all tables are loaded in properly
 cursor.execute("SHOW TABLES")
+for x in cursor:
+    print(x)
 
 
 
